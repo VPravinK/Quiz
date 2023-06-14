7cd55ef0eaf0ae293824696627f3f0870f0ca5b0
unknown <pk@gmail.com>
2023-06-14 17:02:27 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './App.css';
+import questions from './Components/quiz';
+import { useState } from 'react';
+function App() {
+  const[questionIndex,setQuestionIndex]=useState(0);
+  const[result,setResult]=useState(false);
+  const[score,setScore]=useState(0);
+  const replay =()=>{
+    setQuestionIndex(0);
+    setResult(false);
+    setScore(0);
+  }
+  const present_question= questions[questionIndex];
+  const get_choice =(index)=>{
+    if(present_question.Answer===index){
+      setScore(score+1);
+    }
+    const next_question=questionIndex+1;
+    if(next_question<questions.length)
+    {
+      setQuestionIndex(next_question);
+    }
+    else{
+      setResult(true);
+    }
+  };
+  return (
+    <div className="quiz_box">
+      {result?(<><h1>{score}</h1>
+      <br/><p>
+      <button className="replay"onClick={replay}>Play Again</button></p></>):(<div className='quiz_qus'>
+      {present_question.question}
+        <div className='options'>
+      <ul className='quiz_ul'>
+         { present_question.options.map((option,i) =>
+          {return <li className='quiz_li' onClick={()=> get_choice(i)}>{option}</li>})}
+      </ul>
+      </div>
+     </div>)}
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/quiz.js
@@ -0,0 +1,42 @@
+
+const questions= 
+[
+    {
+        question: "What is the full form of CSS?",
+
+        options:["cascading style sheet","cad style sheet","cascade sheet styles","none of above"],
+        Answer: 0,
+    },    
+    {
+        question: "2nd one",
+        options:["A","B","C","D"],
+        Answer: 2,
+    },
+    {
+        question: "3rd one",
+        options:["A","B","C","D"],
+        Answer: 0,
+    },
+    {
+        question: "4th one",
+        options:["A","B","C","D"],
+        Answer: 3,
+    },
+    {
+        question: "5th one",
+        options:["A","B","C","D"],
+        Answer: 2,
+    },
+    {
+        question: "6th one",
+        options:["A","B","C","D"],
+        Answer: 0,
+    },
+    {
+        question: "7th one",
+        options:["A","B","C","D"],
+        Answer: 1,
+    }
+]
+
+export default questions;
