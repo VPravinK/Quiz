9a18ac6ed138580f7ff29523bae6df5ef7dc8ca1
unknown <pk@gmail.com>
2023-06-14 17:03:42 +0530

    basic Quiz questions

--- src/Components/quiz.js
+++ src/Components/quiz.js
@@ -1,42 +1,45 @@
 
 const questions= 
 [
-    {
-        question: "What is the full form of CSS?",
 
-        options:["cascading style sheet","cad style sheet","cascade sheet styles","none of above"],
-        Answer: 0,
-    },    
-    {
-        question: "2nd one",
-        options:["A","B","C","D"],
-        Answer: 2,
-    },
-    {
-        question: "3rd one",
-        options:["A","B","C","D"],
-        Answer: 0,
-    },
-    {
-        question: "4th one",
-        options:["A","B","C","D"],
-        Answer: 3,
-    },
-    {
-        question: "5th one",
-        options:["A","B","C","D"],
-        Answer: 2,
-    },
-    {
-        question: "6th one",
-        options:["A","B","C","D"],
-        Answer: 0,
-    },
-    {
-        question: "7th one",
-        options:["A","B","C","D"],
-        Answer: 1,
-    }
+        {
+            question: "What is the full form of CSS?",
+    
+            options:["cascading style sheet","cad style sheet","cascade sheet styles","none of above"],
+            Answer: 0,
+        },     {
+            question: "Howmany colors in a Rainbow?",
+            options:["6","8","7","9"],
+            Answer: 2,
+        },
+        {
+            question: "Latest Version of HTML is __?",
+            options:["V-5","V-4","V-4.8","V-5.1"],
+            Answer: 0,
+        },
+        {
+            question: "Which component is used to compile, debug and execute the JAVA programs?",
+            options:["JRE","JIT","IDE","JDK"],
+            Answer: 3,
+        },
+        {
+            question: "What is the Extension of compiled java classes?",
+            options:[".txt",".js",".class",".java"],
+            Answer: 2,
+        },
+        {
+            question: "JSX converts?",
+            options:["HTML into React Elements","JS into HTML","XML into react elements","JS into React elements"],
+            Answer: 0,
+        },
+        {
+            question: "HTML is a ___language?",
+            options:["Scripted","Markup","Dynamic","Programming"],
+            Answer: 1,
+        },
+     
+      
+        
 ]
 
 export default questions;
